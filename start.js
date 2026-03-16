1c14db5a978b05cac2f94f7d93afe8a96a277ee1
cocktailpeanut <cocktailpeanuts@proton.me>
2026-03-16 01:39:04 -0400

    stop gateway on start

--- start.js
+++ start.js
@@ -1,27 +1,28 @@
 module.exports = {
-    daemon: true,
-    run: [{
-      "method": "shell.run",
-      "params": {
-        "message": [
-          "openclaw gateway run"
-        ],
-        "on": [{
-          "event": "/listening on.*ws:\\/\\/([0-9.:]+)/",
-          "done": true
-        }]
-      }
-    },
-    {
-      "method": "shell.run",
-      "params": {
-        "message": [
-          "openclaw dashboard"
-        ],
-        "on": [{
-          "event": "/http:\\/\\/[^ ]+ /",
-          "done": true
-        }]
-      }
-    }]
+  daemon: true,
+  run: [{
+    "method": "shell.run",
+    "params": {
+      "message": [
+        "openclaw gateway stop",
+        "openclaw gateway run"
+      ],
+      "on": [{
+        "event": "/listening on.*ws:\\/\\/([0-9.:]+)/",
+        "done": true
+      }]
+    }
+  },
+  {
+    "method": "shell.run",
+    "params": {
+      "message": [
+        "openclaw dashboard"
+      ],
+      "on": [{
+        "event": "/http:\\/\\/[^ ]+ /",
+        "done": true
+      }]
+    }
+  }]
 }
